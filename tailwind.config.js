dfe6b6fcf0f9fd345f0e384d09d227f4a7be7da1
jonijoke <jonijoke@edu.lapinamk.fi>
2026-01-19 16:39:37 +0200

    Chatbot lisätty

---
+++ tailwind.config.js
@@ -0,0 +1,18 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  prefix: 'tw',
+  content: [
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/**/*.{js,ts,jsx,tsx,mdx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  corePlugins: {
+    
+    preflight: false,
+  },
+  plugins: [],
+}
